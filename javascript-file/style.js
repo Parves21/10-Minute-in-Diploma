8bc28435e6dea5c274145488457d271fe6df55bf
Parves21 <nurislam42021@gmail.com>
2023-03-09 01:08:13 +0600

    department add korlam

--- javascript-file/style.js
+++ javascript-file/style.js
@@ -1,12 +1,9 @@
-console.log('it has added.');
-console.log('Everything is okey !');
-
 const menuBar = () => {
    const menuBarAdd = document.getElementById('menu-bar');
    const createNavMenu = document.createElement('li');
    createNavMenu.innerHTML = `
-      <div class="active ps-5 pe-3 pt-2 fs-5 fw-bold" type="button" data-bs-toggle="offcanvas"
-         data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">Menu</div>
+      <div class="active ps-5 pe-3 fs-5 fw-bold" type="button" data-bs-toggle="offcanvas"
+         data-bs-target="#aboutUsOption" aria-controls="aboutUsOption">About Us</div>
    `;
    menuBarAdd.appendChild(createNavMenu);
 }
@@ -17,7 +14,8 @@ const departmentBar = () => {
    const departmentNavAdd = document.getElementById('department-bar');
    const createDepartmentMenu = document.createElement('li');
    createDepartmentMenu.innerHTML = `
-   <a class="nav-link ps-3 pe-3 fs-5" aria-current="page" href="#">Department</a>
+      <div class="active px-3 fs-5 fw-semibold" type="button"     data-bs-toggle="offcanvas"
+      data-bs-target="#departmentOption" aria-controls="departmentOption">Department</div>
    `;
    departmentNavAdd.appendChild(createDepartmentMenu);
 }
@@ -27,7 +25,8 @@ const coursesBar = () => {
    const coursesNavAdd = document.getElementById('courses-bar');
    const createCoursesMenu = document.createElement('li');
    createCoursesMenu.innerHTML = `
-   <a class="nav-link ps-3 pe-3 fs-5" aria-current="page" href="#">Courses</a>
+      <div class="active px-3 fs-5 fw-semibold" type="button"     data-bs-toggle="offcanvas"
+      data-bs-target="#courseDetailsOption" aria-controls="courseDetailsOption">Courses</div>
    `;
    coursesNavAdd.appendChild(createCoursesMenu);
 }
@@ -37,7 +36,8 @@ const liveBar = () => {
    const liveBarNavAdd = document.getElementById('live-bar');
    const createliveBarMenu = document.createElement('li');
    createliveBarMenu.innerHTML = `
-   <a class="nav-link ps-3 pe-3 fs-5" aria-current="page" href="#">Live Class</a>
+      <div class="active px-3 fs-5 fw-semibold" type="button"     data-bs-toggle="offcanvas"
+      data-bs-target="#liveLinkOption" aria-controls="liveLinkOption">Live Class</div>
    `;
    liveBarNavAdd.appendChild(createliveBarMenu);
 }
